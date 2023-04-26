2054252bf5c2519595854733030bedd3be6852d7
minku-koo <minku.koo@samsung.com>
2023-04-26 20:28:15 +0900

    add new methods

--- libs/utils.py
+++ libs/utils.py
@@ -1,26 +1,59 @@
 ﻿
 import cv2
-import random, datetime, os
-
-def get_job_id():
-    nowTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    job_id = nowTime + str(random.randint(11 , 999999))
-    return job_id
+import numpy as np
 
 # 이미지 투명하게
-def setBackgroundAlpha(painted_map, numbered_map, alpha = 0.15):
+def set_alpha_on_background_image(base_image, wrap_image, alpha = 0.2):
     '''
     Parameters
-        painted_map <np.ndarray> : Painted Map
-        numbered_map <np.ndarray> : Nummberring in Line Map
-        alpha <float> : alpha value (default = 0.15)
-    returns
-        painted_map applied alpha + numbered_map <np.ndarray>
-    '''
-    
-    return cv2.addWeighted(painted_map, alpha, numbered_map, (1-alpha), 0, dtype = cv2.CV_32F)
+    ----------
+    base_image : np.ndarray
+        Base image
+    wrap_image : np.ndarray
+        Wrap image on base image
+    alpha : float, optional (default: 0.2)
+        Alpha value apply to base image
 
-# BGR to CYMK , Mixing ratios
-# 21.07.17
-def ratio_brg2cymk(blue, green, red):
-    return 0
+    Returns
+    ----------
+    output : np.ndarray
+        Base image applied alpha + wrap_image
+    '''
+    output = cv2.addWeighted(base_image, alpha, wrap_image, (1 - alpha), 0, dtype = cv2.CV_32F)
+    return output
+
+
+
+# BGR Color tuple convert to Hex Color String Code
+def __bgr2hex(bgr):
+    b, g, r = bgr
+    return ('%02x%02x%02x' % (b, g, r)).upper()
+    
+# Hex Color String Code convert to BGR Color np.array
+def __hex2bgr(hex):
+    return np.array( [int(hex[i:i+2], 16) for i in (4, 2, 0)] ) 
+
+
+
+# counting numbers of color
+def getNumberOfColor(image):
+    """
+    Parameters
+        image <np.ndarray> : image
+    returns
+        numbers of color on image <int>
+    """
+    colorDict = {} # Key : Color Code / Values : Pixel Position
+    for y, row in enumerate(image):
+        for x, bgr in enumerate(row):
+            bgr = tuple(bgr) # np.ndarray convert to tuple
+            if colorDict == {}: # if dictionary in empty
+                colorDict[ bgr ] = [ (y, x) ]
+                continue
+            
+            if bgr in colorDict.keys(): #if pixel color is in dictionary key
+                colorDict[bgr].append( (y, x) )
+            else:
+                colorDict[bgr] = [ (y, x) ]
+            
+    return len(colorDict.keys())
