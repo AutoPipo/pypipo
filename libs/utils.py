1ef1b7c4ce6a7eaf6eac04717590f4b8aea98fc9
minku-koo <corleone@kakao.com>
2023-04-27 13:38:28 +0900

    modify utils methods

--- libs/utils.py
+++ libs/utils.py
@@ -25,35 +25,41 @@ def set_alpha_on_background_image(base_image, wrap_image, alpha = 0.2):
 
 
 # BGR Color tuple convert to Hex Color String Code
-def __bgr2hex(bgr):
+def bgr2hex(bgr):
     b, g, r = bgr
     return ('%02x%02x%02x' % (b, g, r)).upper()
     
 # Hex Color String Code convert to BGR Color np.array
-def __hex2bgr(hex):
+def hex2bgr(hex):
     return np.array( [int(hex[i:i+2], 16) for i in (4, 2, 0)] ) 
 
 
 
 # counting numbers of color
-def getNumberOfColor(image):
-    """
+def get_number_of_image_color(image):
+    """Get number of image colors
     Parameters
-        image <np.ndarray> : image
-    returns
-        numbers of color on image <int>
+    ----------
+        image : np.ndarray
+            Input image
+
+    Returns
+    ----------
+        number_of_colors : int
+            Number of colors
     """
     colorDict = {} # Key : Color Code / Values : Pixel Position
     for y, row in enumerate(image):
         for x, bgr in enumerate(row):
             bgr = tuple(bgr) # np.ndarray convert to tuple
             if colorDict == {}: # if dictionary in empty
-                colorDict[ bgr ] = [ (y, x) ]
+                colorDict[ bgr ] = [(y, x)]
                 continue
             
             if bgr in colorDict.keys(): #if pixel color is in dictionary key
-                colorDict[bgr].append( (y, x) )
+                colorDict[bgr].append((y, x))
             else:
-                colorDict[bgr] = [ (y, x) ]
-            
-    return len(colorDict.keys())
+                colorDict[bgr] = [(y, x)]
+
+    number_of_colors = len(colorDict.keys())  
+    return number_of_colors
