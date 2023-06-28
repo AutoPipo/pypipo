a19b958bf8263f867582967732c669171aa2afd7
Minku Koo <koomk97@naver.com>
2023-06-29 01:16:54 +0900

    remove some parameter, update cli
    - update cli function : add options, argument
    - add save method at process classes
    - blurring was options, but update to always

--- pypipo/convert.py
+++ pypipo/convert.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 
-def convert_image(filepath, **kwargs):
+from libs.process import Painting, LineDrawing
+
+def convert_image(filepath, outputpath, **kwargs):
     print('=== convert image ===')
-    print(filepath)
-    print(kwargs)
-    #######
-    '''
-    img = cv2.imread("./libs/lala.jpg")
-    painting = Painting(img)
-    painting_image, color_index_map = painting.run(
-                                                k = 8,
-                                                is_upscale = True,
-                                                size = 2,
-                                                blurring = True)
+    
+    painting = Painting(filepath)
+    p, color_index_map = painting.run(**kwargs)
     
     drawing = LineDrawing(color_index_map)
     line_drawn_image = drawing.run(outline = True)
-    cv2.imwrite("./libs/lala-after-line-drawn.jpg", line_drawn_image)
-    '''
+    drawing.save(outputpath, line_drawn_image)
+    
     return 
 
