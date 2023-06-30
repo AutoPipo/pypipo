da377d74cb647091da8ad15790c5ce8a786bdf55
Minku Koo <koomk97@naver.com>
2023-07-01 03:50:00 +0900

    add function for colorspace indexing
    - add new function for colorspace indexing

--- pypipo/convert.py
+++ pypipo/convert.py
@@ -6,10 +6,13 @@ def convert_image(filepath, outputpath, **kwargs):
     print('=== convert image ===')
     
     painting = Painting(filepath)
-    p, color_index_map = painting.run(**kwargs)
+    painting_img, color_index_map = painting.run(**kwargs)
+    colorNames_, colors_ = painting.getColorFromImage(painting_img)
+
     
     drawing = LineDrawing(color_index_map)
     line_drawn_image = drawing.run(outline = True)
+    img_lab, lab = drawing.getImgLabelFromImage(colors_, painting_img)
     drawing.save(outputpath, line_drawn_image)
     
     return 
