19843a7f5a25eb6cef3557110bced74d1ab7cb0b
Minku Koo <koomk97@naver.com>
2023-07-02 00:34:08 +0900

    add numbering module in covert function
    - add numbering process

--- pypipo/convert.py
+++ pypipo/convert.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from libs.process import Painting, LineDrawing
+from libs.process import Painting, LineDrawing, ColorspaceIndexing
+from libs.utils import *
 
 def convert_image(filepath, outputpath, **kwargs):
     print('=== convert image ===')
+    print(outputpath)
     
     painting = Painting(filepath)
     painting_img, color_index_map = painting.run(**kwargs)
-    colorNames_, colors_ = painting.getColorFromImage(painting_img)
+    # TODO: change values name
+    color_indexs, color_rbg_values = painting.get_clustered_color_info(painting_img)
 
     
     drawing = LineDrawing(color_index_map)
     line_drawn_image = drawing.run(outline = True)
-    img_lab, lab = drawing.getImgLabelFromImage(colors_, painting_img)
-    drawing.save(outputpath, line_drawn_image)
+    # TODO: change values name
+    img_lab, lab = drawing.get_image_label(color_rbg_values, painting_img)
+
+    print('=== Numbering ===')
+    numbering = ColorspaceIndexing(painting_img, line_drawn_image, color_indexs, color_rbg_values)
+    # TODO: add cli, color_label parameter
+    output = numbering.run(img_lab, lab, color_label = True)
+    img_save(outputpath, output)
     
     return 
 
