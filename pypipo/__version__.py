427d29398a9f2d40c02251cc15d563f5c7a3e7d8
Minku Koo <koomk97@naver.com>
2023-07-02 23:04:19 +0900

    Update __version__.py

--- pypipo/__version__.py
+++ pypipo/__version__.py
@@ -16,6 +16,6 @@ __title__ = "pypipo-py"
 __description__ = "Image convert to PIPO painting canvas automatically."
 __url__ = "https://github.com/AutoPipo/pypipo"
 __version__ = generate_version(VERSION, prerelease=PRERELEASE, revision=REVISION)
-__author__ = ("Minku-Koo", "Jiyong-Park")
-__author_email__ = "corleone@kakao.com",
+__author__ = "Minku-Koo", "Jiyong-Park"
+__author_email__ = "corleone@kakao.com", "comjiyong@kakao.com"
 __license__ = "MIT License"
