e7bd92c6d396fe464672429edb137d3fb2f30c42
Minku Koo <corleone@kakao.com>
2023-12-31 17:05:02 +0900

    Update sample code

--- sample_code.py
+++ sample_code.py
@@ -2,12 +2,17 @@ import cv2
 from pypipo.convert import pipo_convert, pipo_convert_dev
 
 def sample_of_pypipo():
+    sample_image_binary = cv2.imread(f'./pypipo/sample/sample-image.png')
+    pipo_output_binary = pipo_convert(sample_image_binary, number = 16, is_upscale = True)
+    return pipo_output_binary
+
+def sample_of_pypipo_dev():
     number_of_repeat = 3
     for i in range(number_of_repeat):
         # sample_image_binary = cv2.imread(f'./pypipo/sample/sample-image.png')
         
         if i == 0:
-            sample_image_binary = cv2.imread(f'./pypipo/sample/image-0.png')
+            sample_image_binary = cv2.imread(f'./pypipo/sample/image-{i}.png')
             pipo_output_binary = pipo_convert_dev("./pypipo/sample/", 
                                           sample_image_binary, 
                                           number = 20,
@@ -21,4 +26,5 @@ def sample_of_pypipo():
     return 
 
 if __name__ == "__main__":
-    sample_of_pypipo()
+    # sample_of_pypipo()
+    sample_of_pypipo_dev()
